b64c741f27b8428990307cec1af9df6c7d753045
jeff mihalsky <jmihalsky@gmail.com>
2019-01-13 00:28:42 -0800

    mongo connection change

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/homework18", { useNewUrlParser: true});
+var CONNECTION_URI = process.env.MONGODB_URI || "mongodb://localhost/homework18"
+
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true});
 
 mongoose.connection.once("open",function(){
     console.log("Connection made");
